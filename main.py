c5a032359574e75ea66fdb40c55c94686af2904f
rorld4836-bit <rorld4836@gmail.com>
2026-02-09 17:04:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 Привет! Это тестовый battle-бот.\n\nКоманды:\n/start — начать"
+    )
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
